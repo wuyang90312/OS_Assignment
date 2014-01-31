a4f4e4cd2f3bfb7b50c39edcbd938077caefdaa2
Yang <wuyangcanada@hotmail.com>
2014-01-30 23:57:36 -0500

    Update Assignment2_260290265.c
    Add location and size of text, data, bss segments

--- Assignment2/Assignment2_260290265.c
+++ Assignment2/Assignment2_260290265.c
@@ -6,32 +6,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static const int something = 100;
+static char* dummy = "blablabla I don't what to say";
+static char some[50];
 extern char etext, edata, end; /* 	The symbols mus have some type,
 									or "gcc -Wall" complians*/
-void printfMore()
-{
-	printf(" Address of the main func	%10p\n", &printfMore);
-}
+void printfMore();
 
 int main(int argc, char *argv[])
 {
 	int x;
-	label:
-	
+	int addr[4], size[3]; /* Find all of four points and three sizes in between text, data, bss segments */
+
+	/* Take record of every single point at each end of each segment*/
+	addr[0] = (int) &main; /* Here, Assume the address of the main is the starting point of the text segment*/
+	addr[1] = (int) &etext;
+	addr[2] = (int) &edata;
+	addr[3] = (int) &end;
+
+	/* Take record of the sizes of three segments*/
+	size[0] = addr[1] - addr[0]; /* An important assumption: three segments are adjacent*/
+	size[1] = addr[2] - addr[1];
+	size[2] = addr[3] - addr[2];
 	printf("First address past:\n");
-	printf(" program text (etext)	%10p\n", &etext);	
-	printf(" initialized data (edata)	%10p\n", &edata);
-	printf(" uninitialized data (end)	%10p\n", &end);
-	printf(" Address of the main func	%10p\n", &main);
-	printf("location of variable x		%10p\n", &x);
-	printf("location of a label		%10p\n", &&label);
-	
+
+	printf(" Address of the main func	%x\n", addr[0]);
+	printf(" program text (etext)	%x -> %x\n", addr[1], size[0]);
+	printf(" initialized data (edata)	%x -> %x\n", addr[2], size[1]);
+	printf(" uninitialized data (end)	%x -> %x\n", addr[3], size[2]);
+
+
 	label2:
-	
-	printfMore();
+
+	//printfMore();
 	printf("location of a label	2	%10p\n", &&label2);
-	
+
 	return 0;
 }
 
+void printfMore()
+{
+	printf(" Address of the printMore func	%10p\n", &printfMore);
+	labe3:
+	printf("location of labe 3      %10p\n", &&labe3);
+}
 
