28a3c1305943943f014ef4102b992e31344d1af4
Yang <wuyangcanada@hotmail.com>
2014-02-05 13:37:09 -0500

    Update Assignment2_260290265.c
    Finished

--- Assignment2/Assignment2_260290265.c
+++ Assignment2/Assignment2_260290265.c
@@ -12,31 +12,23 @@
 #include <alloca.h>
 #include <signal.h>
 
-
+static int *glob_var; /*create a global variable*/
 static const int something = 100;
 static char* str = "The File is Printing Below:\n";
 static char some[50];
 extern char etext, edata, end; /* 	The symbols mus have some type,
 									or "gcc -Wall" complians*/
-int checkStack(int);
-int checkStack(int index);
-void handle_SIGSEGV();
+int checkHeap(int index);
 
 int main(int argc, char *argv[])
 {
-	struct sigaction sa;
-
-    sa.sa_flags = SA_SIGINFO;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_sigaction = handle_SIGSEGV;
-    if (sigaction(SIGSEGV, &sa, NULL) == -1)
-    {
-		
-	}
-	
+/*********************************************************************************************************/
+	pid_t pid;
 	int addr[5], size[4]; /* Find all of four points and three sizes in between text, data, bss, File Mapping segments */
 	int addr_Stk, size_Stk, addr_Hp, size_Hp; /* Define varaibles for addresses and sizes of Stack and Heap */
-	int addition = 0xfffffff;
+	int addition = 0x10000000; /* set up the addition value to a large number*/
+
+/*****************Text, Data, BSS segments***************************************/	
 	/* Take record of every single point at each end of each segment*/
 	addr[0] = (int) &main; /* Here, Assume the address of the main is the starting point of the text segment*/
 	addr[1] = (int) &etext;
@@ -72,80 +64,64 @@ int main(int argc, char *argv[])
 	char *ptr =	(char*)mmap(NULL, filestat.st_size,	PROT_READ, MAP_PRIVATE,	fd,	0);		
 	addr[4] = (int) ptr;
 	size[3] = filestat.st_size;
-	//	Print the	file..	
-	int	i;	
-	printf("%s", str);
-	for(i=0;i<filestat.st_size;	i++)
-	{
-		putchar(ptr[i]);
-	}
 	
 	munmap(ptr,	filestat.st_size);	
-/*****************Text, Data, BSS segments***************************************/	
-	printf(" section			Address		size\n");
-
-	//printf(" Address of the main func		0x%x\n", addr[0]);
-	printf(" Text segment			0x%x 	%x\n", addr[0], size[0]);
-	printf(" initialized data segment	0x%x 	%x\n", addr[1], size[1]);
-	printf(" uninitialized data segment	0x%x	%x\n", addr[2], size[2]);
-	printf(" file mapping segment		0x%x	%x\n", addr[4], size[3]);
 /*****************Heap and Stack segments***************************************/	
 	/*Get the starting point of the Stack*/
-	/*i = 0;
-	addr_Stk = (int) alloca(0);
-	printf("the starting point is %x  - %c\n", addr_Stk, &addr_Stk);
-	printf("the output  is %x\n", alloca(0xfffff));
-	//printf("the starting point is %x\n", alloca(0xffffe));
-	while(!checkStack(i)){
-		i+=16;
-		//printf("run\n");
-		printf("the size is %x\n", i);
-	}
-	size_Stk = i;
-	printf("the size is %x\n", size_Stk);*/
-
-	i = 0;
+	int	i = 0;
 	addr_Hp = (int) malloc(0);
-	printf("the starting point is %x  - %c\n", addr_Hp, &addr_Hp);
-	while(addition != 1){
-		if(!checkHeap(i))
+	while(addition != 1){ /* once if addition reaches 1, stop while loop */
+		if(!checkHeap(i)) /* once malloc overflows, reduce the addition value by a factor of 2 */
 		{
-			i -= addition;
+			i -= addition; /* restore the value before overflow*/
 			addition = addition /2;
 		}
 		i+= addition;
 	}
 	size_Hp = i;
-	printf(" Heap Sement			0x%x	%x\n", addr_Hp, size_Hp);
 	free(addr_Hp);
-/*******************************************************************************/	
-
-	return 0;
-}
-
-int checkStack(int index)
-{
-	int addr = alloca(index);
-	//printf("the output  is %x\n", alloca(index));
-	if(!alloca(0xfffffffff))
-	{
-		printf("hahaha\n");
-		return 1;
+	
+	glob_var = mmap(NULL, sizeof *glob_var, PROT_READ | PROT_WRITE, 
+                    MAP_SHARED | MAP_ANONYMOUS, -1, 0); /* create a global varibles between two processes*/
+    *glob_var = 1;
+    i = 0;
+	addr_Stk = (int) alloca(0);
+	
+	printf("Process ");
+	pid = fork();
+	if(pid == 0){
+		char* addr_stk = NULL;
+		while(1){
+			addr_stk = alloca(1);
+			i++;
+			*glob_var = i;
+			printf(".");
+		}
+		exit(EXIT_SUCCESS);
 	}
 	else
-		return 0;
+	{
+		wait(NULL);
+		size_Stk = *glob_var-1;
+		munmap(glob_var, sizeof *glob_var);
+	}
+/*****************Print Text, Data, BSS, and mapping segments*******************/	
+	printf("\n section			starting Address	size\n");
+	printf(" Text segment			0x%x 		%x\n", addr[0], size[0]);
+	printf(" initialized data segment	0x%x 		%x\n", addr[1], size[1]);
+	printf(" uninitialized data segment	0x%x		%x\n", addr[2], size[2]);
+	printf(" file mapping segment		0x%x		%x\n", addr[4], size[3]);
+	printf(" Heap Segment			0x%x		%x\n", addr_Hp, size_Hp);
+	printf(" Stack Segment			0x%x		%x\n", addr_Stk, size_Stk);
+/*******************************************************************************/	
 
+	exit(EXIT_SUCCESS);
 }
 
-int checkHeap(int index)
+int checkHeap(int index) /* Check if the Heap is overflowed */
 {
 	int* addr = (int*) malloc(index);
 	free(addr);
-	return addr;
+	return addr; /* if malloc overflows, return 0 */
 		
-}
-
-void handle_SIGSEGV()
-{
-	fprintf( stderr, "MEM is OUT\n");
 }
