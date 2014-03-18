174c498437cab30ca666a9fa963d02e3668cd440
Yang <wuyangcanada@hotmail.com>
2014-03-18 17:27:14 -0400

    Update Assignment4_260290265.c
    Almost finished the Banker's Algorithm -- arrange the request -> run -> release -> request ...

--- Assignment4_260290265.c
+++ Assignment4_260290265.c
@@ -10,16 +10,33 @@
 
 #define AVOIDANCE	"avoidance"
 #define DETECTION	"detection"
+#define SUCCESS		1
+#define FAILURE		0
+#define SEED		3
+#define MINUS		0
+#define PLUS		1
 
-static int threadNum, resourceType;
+static int threadNum, resourceType, process, processTime;
 static int *globalCond;
+static int *processCond;
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t mutexNUM = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t mutexCOND = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t mutexADD = PTHREAD_MUTEX_INITIALIZER;
 static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;	
 
+
+/* For testing purpose */
+int processlimit[4][3] ={{4,4,4},{4,4,4},{4,4,4},{4,4,4}}; 
+ 
 /*
  *********************Declaration of all the classes********************/
-int Banker(int,int);
 void* threadCreation(void*);
+void sleepProcess();
+ void globalCondModify(int, int, int);
+int checkSafty();
+int Banker(int, int,int);
+int resourceRequest(int, int, int, int);
 int allocateResources(int, int*);
  /*
   ********************End of the Declaration****************************/
@@ -28,14 +45,14 @@ int main(int arg, char* argv[]) /* Todo: to finishe the assignment, add more typ
 {
 	int sign, loop, tmp;
 	pthread_attr_t attr;
-		
+
 /******************Check the states of arguments & Initialize the global variables************/	
 	if(arg < 5) /*If the argument is less than 4, the argument is not enough*/
 	{
 		fprintf(stderr, "ERROR: Lack of Arguments!!!\n");
 		return EXIT_FAILURE;
 	}
-	
+
 	resourceType = arg-4; /* Intialize the condition variables*/
 	globalCond = malloc(resourceType*sizeof(int));
 	for(loop = 3; loop < arg-1;  loop++)
@@ -46,6 +63,7 @@ int main(int arg, char* argv[]) /* Todo: to finishe the assignment, add more typ
 			fprintf(stderr, "ERROR: Invalid of Argument: \nArgument %d is supposed to be a positive integer\n", loop);
 			return EXIT_FAILURE;
 		}
+		
 	}
 
 /*************************Initialize the global variables********************************/
@@ -55,9 +73,21 @@ int main(int arg, char* argv[]) /* Todo: to finishe the assignment, add more typ
 		fprintf(stderr, "ERROR: Invalid of Argument: \nArgument 2 is supposed to be a positive integer\n");
 		return EXIT_FAILURE;
 	}
-
+	
+	processTime = atoi(argv[arg-1]);
+	if(!processTime)
+	{
+		fprintf(stderr, "ERROR: Invalid of Argument: \nArgument %d is supposed to be a positive integer\n", (arg-1));
+		return EXIT_FAILURE;
+	}
+	printf("the time is %d\n", processTime);
+	processCond = malloc(resourceType*threadNum*sizeof(int)); /* create a */
 	/*Create a pthread according to the set number*/
 	pthread_t thread[threadNum];
+	for( loop =0; loop <resourceType*threadNum; loop++) //TODO: clean up the for loop
+	{
+		processCond[loop] = processlimit[loop/3][loop%3];
+	}
 
 /***********************Decide the type of resource allocator****************************/
 	if(strcasecmp(DETECTION,argv[1]) && strcasecmp(AVOIDANCE,argv[1]))
@@ -82,33 +112,157 @@ int main(int arg, char* argv[]) /* Todo: to finishe the assignment, add more typ
 			exit(sign);
 	}
 	printf("2 succuess\n"); //todo: clean up the sentence
-	sleep(2);
+	sleep(processTime);
 	return EXIT_SUCCESS;
 }
 
-int Banker() /*The whole section is set as a critical session so that only one process can access the Banker at the same time*/
+int Banker(int currentprocess, int type, int resource) /*The whole section is set as a critical session so that only one process can access the Banker at the same time*/
 {
+	int rc, safty;
 	rc = pthread_mutex_lock(&mutex);
-	printf("the resources are %d  %d  %d \n", globalCond[0],globalCond[1],globalCond[2]);
+	//printf("The %d-%d\t", currentprocess, type);
+	//printf("1. the resources are %d  %d  %d \t", globalCond[0],globalCond[1],globalCond[2]);
 	
+	//globalCond[type] -= resource;
+	globalCondModify(MINUS,resource,type);
+	processCond[currentprocess*resourceType+type] -=resource;
 	
+	safty = checkSafty(); /* check the safty of the resources*/
+	if(!safty) /* Once the whole system is not safe after assignment, reject the action, back up the old data */
+	{
+		//globalCond[type] += resource;
+		globalCondModify(PLUS,resource,type);
+		processCond[currentprocess*resourceType+type] +=resource;
+	}
+	//printf("2. the resources are %d  %d  %d --> %d\n", globalCond[0],globalCond[1],globalCond[2],safty);
 	rc = pthread_mutex_unlock(&mutex);
-	return EXIT_SUCCESS;
+	return safty;
+}
+
+int checkSafty()
+{	
+	int loop, succession, quotient; 
+	loop = 0;
+	succession = 0;
+
+	while(loop < resourceType*threadNum)
+	{
+		if(globalCond[loop%resourceType] >= processCond[loop])
+		{
+			//printf("%d >= %d\t", globalCond[loop%resourceType], processCond[loop]);
+			succession ++;
+			if(succession == 3)
+			{
+				return SUCCESS;
+			}
+			loop ++;
+		}else
+		{
+			succession = 0;
+			quotient = (int) (loop / resourceType);
+			loop = (quotient+1)*resourceType;
+		}
+			
+	}
+	
+	return FAILURE;
 }
 
 void* threadCreation(void* args) /*Now Assume that the resource is only one type; TODO: accomodate with multiple resources*/
  {
+	 int algorithm, currentprocess,rc, type[resourceType], resource[resourceType];
+
+	 
+	 rc = pthread_mutex_lock(&mutexNUM); /* Index each of the processes*/
+	 currentprocess = process;
+	 process ++;
+	 rc = pthread_mutex_unlock(&mutexNUM);
+
 	 if(!strcasecmp(AVOIDANCE,(char*) args))
 	 {
-		 printf("start to use banker's algorithm\n");
-		 if(Banker(0,1))
-		{
-			fprintf(stderr, "ERROR: Failure of Banker's Algorithm!!!\n");
-			pthread_exit(0); /* exit thread */
-		}
+		 algorithm = 0;
 	 }else
 	 {
-		 printf("start to use detection algorithm\n");
+		 algorithm = 1;
 	 }
+	 sleepProcess();
+	 type[0] = 0;
+	 resource[0] = processlimit[currentprocess][0];
+	 if(!resourceRequest(currentprocess, algorithm, type[0], resource[0]))
+	 {
+		fprintf(stderr, "ERROR: Failure of Resource Allocation!!!\n");
+		pthread_exit(0);
+	 }
+	sleepProcess();
+	 type[1] = 1;
+	 resource[1] = processlimit[currentprocess][1];
+	 if(!resourceRequest(currentprocess, algorithm, type[1], resource[1]))
+	 {
+		fprintf(stderr, "ERROR: Failure of Resource Allocation!!!\n");
+		pthread_exit(0);
+	 }
+	sleepProcess();
+	 type[2] = 2;
+	 resource[2] = processlimit[currentprocess][2];
+	 if(!resourceRequest(currentprocess, algorithm, type[2], resource[2]))
+	 {
+		fprintf(stderr, "ERROR: Failure of Resource Allocation!!!\n");
+		pthread_exit(0);
+	 }
+
+	/* keeping the thread running until program terminate itself */
+	while(TRUE)
+	{
+		int seed = time(NULL);
+		srand(seed);
+		int resourceT = rand()%resourceType;
+		if(!resourceRequest(currentprocess, algorithm, resourceT, resource[2]))
+		{
+			
+		}
+		sleepProcess();
+	}
+
+	 globalCond[0] += resource[0];
+	 globalCond[1] += resource[1];
+	 globalCond[2] += resource[2];
+	 pthread_cond_broadcast(&cond); /* once resource are released, wake up all the sleeping threads*/
+	 printf("%d sucessfully start to signal, resource: %d %d %d \n", currentprocess,globalCond[0],globalCond[1],globalCond[2]);
 	 pthread_exit(0); /* exit thread */
+ }
+ 
+ int resourceRequest(int currentprocess, int algorithm, int type, int resource)
+ {
+	 if(algorithm){
+		
+	 }else
+	 {
+		 while(!Banker(currentprocess, type, resource))
+		{
+			//printf(" The %d-%d is stuck for signal \n", currentprocess, type);
+			pthread_cond_wait(&cond, &mutexCOND);
+			pthread_mutex_unlock(&mutexCOND);
+			
+		}
+	 }
+
+	 return SUCCESS;
+ }
+ 
+ void globalCondModify(int type, int content, int index) /* Make sure that only one process modify the global variable each time*/
+ {
+	 pthread_mutex_lock(&mutexADD);
+	 if(type)
+		globalCond[index] += content;
+	 else
+		globalCond[index] -= content;
+	 pthread_mutex_unlock(&mutexADD);
+ }
+ 
+ void sleepProcess()
+ {
+	 srand(SEED);
+	 int sleeptime = rand()%5+1;/* Assume the maximal sleeping time is 4 seconds*/
+	 printf("The sleeping time is %d\n", sleeptime);
+	 sleep(sleeptime);
  }
