1eb4efe5ffc7bba1926f9ccc4ac2dc281f60f11d
SpicyOnion <jeffrey.pyl@outlook.com>
2014-04-08 09:38:55 -0400

    n/a

--- Assignment4_260402139.c
+++ Assignment4_260402139.c
@@ -1,3 +1,9 @@
+/*
+Jeffrey Leung
+260402139
+A4
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -33,7 +39,6 @@ arg 5: amount of time needed for each process
 static int num_res, t_num, p_time, p_count=0, r1_aval = 3, r2_aval = 2, x, y, mode, holdFlag = 0;
 static int *all_cond;
 static int *p_cond;
-static int r_array[2][3]={{1,1,1},{1,1,0}}; // resources array with two types of resources 1 is avalible, 0 is not avalible
 static pthread_mutex_t mutex_stra = PTHREAD_MUTEX_INITIALIZER;
 static pthread_mutex_t mutex_det = PTHREAD_MUTEX_INITIALIZER;
 static int hold_1[100];
@@ -120,27 +125,14 @@ arg 5+: resource needed / resource id
 	
 	pthread_t thread[t_num];
 	
-	// if( strcasecmp(DECTECT,argv[1]) && strcasecmp(AVOID, argv[1]) ) {
-	// 	printf("Invalid input on strategy\n");
-	// 	exit(0);
-	// }
-	int dFlag = 0;
+
 	for(i = 0; i < t_num; i++){
-	//while(1){
 
 		attr_int = pthread_attr_init(&attr);
 		if(attr_int != 0){
 			printf("attritbute initialize unsucessful\n");
 			exit(0);
 		}
-		// if(holdFlag == 1){
-		// 	int temp = p_count;
-		// 	if(mode == DECTECT){
-		// 		temp -= 1;
-		// 	}
-		// 	holdFlag = 0;
-		// 	printf("Process %d on hold.\n", temp+1);
-		// }
 		attr_int = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
 		if(attr_int != 0){
 			printf("attritbute initialize unsucessful\n");
@@ -152,8 +144,7 @@ arg 5+: resource needed / resource id
 			printf("Thread %d creation unsucessful\n",i);
 			exit(0);
 		}
-		
-		//printf("\n");
+		// sleep for appropiate time after each process got their resource allocated
 		sleep(p_time);
 	}
 	
@@ -162,6 +153,9 @@ arg 5+: resource needed / resource id
 	exit(done);
 }
 
+/*
+This method detect if input for mode is input correctly or not and keep count of which process is the thread at.
+*/
 void* stra_sel(void* args){
 	int lock, granted;
 	
@@ -175,14 +169,15 @@ void* stra_sel(void* args){
 
 }
 
+/*
+Sleep for input amount of time
+*/
 void sleep_p(int process_sleep){
 	sleep(process_sleep);
 }
 
 int isSafe(int resource_1_aval, int resource_2_aval, int c_process, int needed_1, int needed_2){
 	int finish[t_num];
-	// int work_1[resource_1_aval];
-	// int work_2[resource_2_aval];
 	int k;
 	for(k = 0; k< t_num;k++){
 		finish[k] = 0;
@@ -192,16 +187,23 @@ int isSafe(int resource_1_aval, int resource_2_aval, int c_process, int needed_1
 		return done;
 	}else if(t_num < -1){
 		printf("Error on the number of process Please check the internal methods\n");
-		exit(0);
+		exit(fail);
 	}
 	if(needed_1 <= resource_1_aval && needed_2 <= resource_2_aval){
 
 	}
 }
 
+/*
+This is the banker algorithm. 
+If there is avalible resources, this method will grant the use of it. 
+Otherwise, hold_1 and hold_2 array store process ID and needed resources for type A and B respectively. This is done by stored needed resouces for type A at hold_1 (B for hold_2)
+at index of the current process ID.
+*/
 int Banker(int req_1, int req_2, int c_proID){
 	
 	if(hold_1[c_proID] == 0 && hold_2[c_proID] == 0){
+		// only print onces for earch proces ID
 		printf("Process %d request number of \nA: %d, B: %d.\nAvalible A: %d B: %d.\n",c_proID,req_1,req_2,r1_aval, r2_aval );
 	}
 	if(req_1 > resouceA || req_2 > resouceB ){
@@ -209,12 +211,14 @@ int Banker(int req_1, int req_2, int c_proID){
 		exit(fail);
 	}else if(hold_1[c_proID] != 0 || hold_2[c_proID] != 0){
 		if(req_1 > r1_aval || req_2 > r2_aval){
-			
+			// when needed resources is within hold arrays but resources still not avalible.
 			Banker(0,0, c_proID);
 		}else{
-
+			// when resources avalible for holded process
 		req_1 = hold_1[c_proID];
 		req_2 = hold_2[c_proID];
+
+		// add back the req value from previous process after its process time (sleep time after thread create in main)
 		r1_aval += req_1;
 		r2_aval += req_2;
 
@@ -230,69 +234,58 @@ int Banker(int req_1, int req_2, int c_proID){
 		return 1;
 		}
 	}else if(req_1 > r1_aval || req_2 > r2_aval){
-
+		// not enough resources and store in hold array, run banker again until resource avalible
 		hold_1[c_proID] = req_1;
 		hold_2[c_proID] = req_2;
 		Banker(0,0, c_proID);
 
 	}else{
-		// if(req_1 == 0 && req_2 == 0){
-		// 	req_1 = hold_1[c_proID];
-		// 	req_2 = hold_2[c_proID];
-		// }
+		// when resource is avalible for the first time
 		r1_aval -= req_1;
 		r2_aval -= req_2;
-
 		hold_1[c_proID] = 0;
 		hold_2[c_proID] = 0;
 		printf("Process %d have been granted resources A: %d, B: %d\n", c_proID, req_1, req_2);
-// IF SAFE?????
-		
+
 		if(r1_aval < req_1 || r2_aval < req_2){
 			holdFlag = 1;
 			}
-		// sleep_p(p_time*10);
-
 		return 1;
 	}
 }
 
+
+/*
+The detection will keep on running within the theread and the the freequency of how often it check deadlock will base on each process time.
+Here if there is deadlock, it will do a recursion of itself until resources is avalible.
+*/
 int dectection(int reqA, int reqB){
 	int sign;
-	
+
 	printf("Process %d request number of \nA: %d, B: %d.\nAvalible A: %d B: %d.\n",p_count,x,y,r1_aval, r2_aval );
-	//int count = 0;
+	if(reqA > resouceA || reqB > resouceB ){
+		printf("Process %d request more resources than maximum resource\n", c_proID);
+		exit(fail);
+	}
 	if(reqA > r1_aval || reqB > r2_aval){
-		
 			sign = pthread_mutex_lock(&mutex_det);
 			holdFlag = 1;
-			//if(count == 0 ){
 			printf("Detect not enough resources for process %d\n", p_count);
+			// since at this point resources is unavlible but after other previous process finish, (same runtime) it resource will be back (after the sleep_p at main after ptherad create)
+			// so add back resource here
 			r1_aval += reqA;
 			r2_aval += reqB;
-			//}	
-			// int times = 0;
-			// if(p_time > 5){
-			// 	times = p_time - 2;
-			// }else{
-			// 	times = p_time;
-			// }
-			// sleep_p(times);
 			sign = pthread_mutex_unlock(&mutex_det);
 			dectection(reqA, reqB);
-
-
-		
 	}else{
-	
+		// when avalible
 		r1_aval -= reqA;
 		r2_aval -= reqB;
-
 		printf("Process %d have been granted resources A: %d, B: %d\n", p_count, reqA, reqB);
 		return 1;
 	}
 }
-
+//Sort into different mode
 int resourceReq(int mode){
 	if(mode == 1){
 		int Bank_sucess = 0;
@@ -301,11 +294,6 @@ int resourceReq(int mode){
 			pthread_exit(NULL);
 			return 0;
 		}
-		//else if(Bank_sucess != 1){
-			// printf("Current Bank_sucess value: %d,\n Process ID: %d\n",Bank_sucess, p_count);
-			// printf("Error on Banker algr!!!!!\n");
-			//Banker(x,y,p_count);
-		//}
 		return 1;
 	}else if(mode == 2){
 		int det = dectection(x,y);
